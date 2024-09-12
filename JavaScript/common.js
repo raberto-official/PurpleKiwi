dbe64419010dcd169be0b8b4d69805cdb92d461b
XxCosmicCloudxX <raberto.official@gmail.com>
2024-09-11 22:11:43 -0700

    Update
    still can't get the date to be anything but null

--- JavaScript/common.js
+++ JavaScript/common.js
@@ -1,49 +1,24 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    // let modalForm = document.getElementById('modalForm');
 
-    // let dateEntry = document.getElementById('dateEntry').value;
+    let dateEntry = document.getElementById('dateEntry');
+    let descEntry = document.getElementById('descEntry');
 
-    // let descEntry = document.getElementById('descEntry').value;
-    // let amountEntry = document.getElementById('amountEntry').value;
-    // let incomeOrDebitEntry = document.getElementById('incomeOrDebitEntry').value;
-    // let paymentMethodEntry = document.getElementById('paymentMethodEntry').value;
-
-    // let addEntryButton = document.getElementById('addEntryButton');
-
-    
-    // function addNewRow(dateEntry, descEntry, amountEntry, incomeOrDebitEntry, paymentMethodEntry) {
-    //     let expensesTable = document.getElementById('expensesTable');
-    //     // adding empty first row
-    //     let row = expensesTable.insertRow(0);
-    //     // insert new cells at positions of the "new" tr element
-    //     let dateCell = row.insertCell(0);
-    //     let descCell = row.insertCell(1);
-    //     let amountCell = row.insertCell(2);
-    //     let incomeOrDebitCell = row.insertCell(3);
-    //     let paymentMethodCell = row.insertCell(4);
-
-    //     // adding data to cells
-    //     dateCell.innerHTML = dateEntry;
-    //     descCell.innerHTML = descEntry;
-    //     amountCell.innerHTML = amountEntry;
-    //     incomeOrDebitCell.innerHTML = incomeOrDebitEntry;
-    //     paymentMethodCell.innerHTML = paymentMethodEntry;
-
-    // };
     document.getElementById('addEntryButton').onclick = function() {
         let table = document.getElementById('expensesTable');
         let row = table.insertRow(-1);
         let dateCell = row.insertCell(0);
         let descCell = row.insertCell(1);
 
-        let dateEntry = document.getElementById('dateEntry');
+        // let dateEntry = document.getElementById('dateEntry');
+        // let descEntry = document.getElementById('descEntry').value;
 
-        dateCell.innerHTML = dateEntry
-        descCell.innerHTML = document.getElementById('descEntry').value;
+        dateCell.innerHTML = dateEntry;
+        descCell.innerHTML = descEntry;
 
         console.log(dateEntry);
+        console.log(descCell)
         
     }
 
         
-});
+});  
