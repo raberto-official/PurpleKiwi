363078d3c8ede4dc55090a3dd5b3cf64e574831a
XxCosmicCloudxX <raberto.official@gmail.com>
2024-09-15 14:33:03 -0700

    Updated Common.js struggling
    Form entries keep coming back as null when button is pressed.

--- JavaScript/common.js
+++ JavaScript/common.js
@@ -1,16 +1,19 @@
 document.addEventListener('DOMContentLoaded', (event) => {
 
-    let dateEntry = document.getElementById('dateEntry');
-    let descEntry = document.getElementById('descEntry');
+
+//Values of every entry keep coming back as Null.
+
+    // let dateEntry = document.getElementById('dateEntry');
+    // let descEntry = document.getElementById('descEntry');
 
     document.getElementById('addEntryButton').onclick = function() {
         let table = document.getElementById('expensesTable');
+        let dateEntry = document.getElementById('dateEntry');
+        let descEntry = document.getElementById('descEntry').value;
+
         let row = table.insertRow(-1);
         let dateCell = row.insertCell(0);
         let descCell = row.insertCell(1);
-
-        // let dateEntry = document.getElementById('dateEntry');
-        // let descEntry = document.getElementById('descEntry').value;
 
         dateCell.innerHTML = dateEntry;
         descCell.innerHTML = descEntry;
